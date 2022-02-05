50ade879e3ef45b3c00f73609bd3c28a7c153a56
Kei Ohnuma <linuxkay@users.noreply.github.com>
2022-02-05 21:08:36 +0900

    Added TCP. Fixed f.write missing "decode".

--- Windows10/silenceisgolden.py
+++ Windows10/silenceisgolden.py
@@ -1,16 +1,18 @@
 # Windows version of silicenisgolden.py Built by linuxkay@
 #!/usr/lcoal/env/python2.7
+# netstat -nao to check socket status
 
 import socket
 import re
 # for lock screen.
 import ctypes
 
-UDP_IP = "192.168.1.22"
-UDP_PORT = port-number
+UDP_IP = "IP"
+UDP_PORT = port_number
 
 sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+                    #socket.SOCK_DGRAM) # UDP
+                     socket.SOCK_STREAM) # TCP
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((UDP_IP, UDP_PORT))
 
@@ -18,5 +20,8 @@ while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("received message:", data)
     # Lock Screen when "lockdown" request received
-    if re.compile(data).search(b'lockdown'):
-                  ctypes.windll.user32.LockWorkStation()
+    if re.compile(data.decode("utf-8")).search('lockdown'):
+                 ctypes.windll.user32.LockWorkStation()
+    f = open(r'C:\Users\User_Name\program_spaces\word.txt', 'w')
+    f.write(data.decode("utf-8"))
+    f.close()
